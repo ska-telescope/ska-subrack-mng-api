a69cd8f19d6f6af96246cdc54628c940f04c93f1
cristian.albanese <cristian.albanese@sanitaseg.it>
2020-06-04 14:58:28 +0200

    update

--- subrack_mng_api/__init__.py
+++ subrack_mng_api/__init__.py
@@ -2,7 +2,5 @@ __author__ = 'Cristian Albanese'
 from subrack_mng_api import management
 from subrack_mng_api import backplane
 from subrack_mng_api import subrack_management_board
-from subrack_mng_api.emulator_classes import backplane_sim
-from subrack_mng_api.emulator_classes import management_sim
 from subrack_mng_api.emulator_classes import def4emulation
 #from subrack_mng_api.subrack_monitor import *
