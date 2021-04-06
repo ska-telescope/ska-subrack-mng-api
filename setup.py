858e2567b86bdba76142cddb03eb0c81406c7fbf
cristian.albanese <cristian.albanese@sanitaseg.it>
2021-04-06 15:33:22 +0200

    add scripts to create virtualenv for python3 and install packages

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='subrack_mng_api',
-    version='1.1',
-    packages=['subrack_mng_api','subrack_mng_api.emulator_classes','cpld_mng_api','cpld_mng_api.netproto','cpld_mng_api.bsp'],
+    version='2.3',
+    packages=['subrack_mng_api', 'subrack_mng_api.emulator_classes', 'cpld_mng_api', 'cpld_mng_api.netproto',
+              'cpld_mng_api.bsp', 'web_server'],
     url='',
     license='GPLv3',
     author='Cristian Albanese',
     author_email='cristian.albanese@sanitaseg.it',
     description='Python API package for subrack management board',
-    dependency_links=['https://bitbucket.org/lessju/pyfabil/get/master.zip#egg=pyfabil'],
-    install_requires=['pyfabil','ipython','psutil'],
+    dependency_links=[],
+    install_requires=['future', 'lxml', 'parse', 'uritools','psutil', ' terminaltables', 'numpy', 'Cython', 'ipython'],
 )
