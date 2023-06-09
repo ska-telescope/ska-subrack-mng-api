0aa01e53f728c6a4efb7dfcfb0a1e62de8143aa7
Gianni Comoretto <giovanni.comoretto@inaf.it>
2023-06-09 22:13:47 +0200

    Added health_status commands to web server

--- web_server/subrack_hardware.py
+++ web_server/subrack_hardware.py
@@ -283,6 +283,51 @@ class TpmInfo(HardwareCommand):
         tpm_slot_id = int(params)
         tpm_info = self._hardware.GetTPMInfo(tpm_slot_id)
         answer["retvalue"] = tpm_info
+<<<<<<< Updated upstream
+=======
+        return answer
+
+class GetHealthDict(HardwareCommand):
+    """
+    Return subrack health status dictionary.
+    """
+
+    def do(self, params):
+        """
+        Info about subrack health status 
+        :param params: group of monitor points to report
+        :type params: str 
+
+        :return:dictionary  of monitor points
+        :rtype: dict
+        """
+        answer = super().do()
+        if (params == "") or (params is None) or (len(params) == 0):
+            answer["retvalue"] = self._hardware.get_health_dict()
+        else:
+            answer["retvalue"] = self._hardware.get_health_dict(group=params)
+        return answer
+
+class GetHealthStatus(HardwareCommand):
+    """
+    Return info about subrack health status.
+    """
+
+    def do(self, params):
+        """
+        Info about subrack health status 
+        :param params: group of monitor points to report
+        :type params: str 
+
+        :return: dictionary of monitor point values
+        :rtype: dict
+        """
+        answer = super().do()
+        if (params == "") or (params is None) or (len(params) == 0):
+            answer["retvalue"] = self._hardware.get_health_status()
+        else:
+            answer["retvalue"] = self._hardware.get_health_status(group=params)
+>>>>>>> Stashed changes
         return answer
 
 
@@ -633,6 +678,12 @@ class SubrackHardware(HardwareThreadedDevice):
         self.add_command(SetFanSpeed("set_subrack_fan_speed", subrack, 2))
         self.add_command(SetPSFanSpeed("set_power_supply_fan_speed", subrack, 2))
         self.add_command(TpmInfo("tpm_info", subrack, 1))
+<<<<<<< Updated upstream
+=======
+        self.add_command(GetHealthDict("get_health_dictionary", subrack, 1))
+        self.add_command(GetHealthStatus("get_health_status", subrack, 1))
+
+>>>>>>> Stashed changes
         # Add attributes
         self.add_attribute(
             BackplaneTemperature("backplane_temperatures", [0] * 2, subrack)
