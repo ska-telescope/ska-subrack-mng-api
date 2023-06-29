7006624c7fd9bf932e63147ace0cdb5c873db438
Gabriele Sorrenti <gabriele.sorrenti@sanitaseg.it>
2023-06-29 19:00:27 +0200

    added git describe to version

--- subrack_mng_api/version.py
+++ subrack_mng_api/version.py
@@ -1,8 +1,17 @@
 __author__ = 'Cristian Albanese'
+import os
+import subprocess
 
 __version__ = '2.2.0'
 
 def get_version():
-    return __version__
+    try:
+        cmd="git -C %s describe --tags --dirty --always"%os.path.dirname(__file__)
+        result = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE)
+        if result.returncode == 0:
+            return __version__ + " (%s)"%str(result.stdout.decode('utf-8').strip())
+        return __version__
+    except:
+        return __version__
 
 
